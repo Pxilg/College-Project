2ec059846e1f112eb35cbf8a884fa6244772ed2e
Pxilg <Pxilg@proton.me>
2026-03-27 11:11:33 +0700

    Add files via upload

---
+++ Project/MindVault/CyberAcademy/RoundedButton.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CyberAcademy
+{
+    public class RoundedButton : Button
+    {
+        // Settings
+        public int BorderRadius { get; set; } = 20;
+        public Color BorderColor { get; set; } = Color.PaleVioletRed;
+        public int BorderSize { get; set; } = 0;
+
+        public RoundedButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.MediumSlateBlue;
+            this.ForeColor = Color.White;
+
+            // Allow us to repaint properly when resized
+            this.Resize += new EventHandler(Container_BackColorChanged);
+        }
+
+        // Helper to create the rounded path
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rectSurface = new RectangleF(0, 0, this.Width, this.Height);
+            RectangleF rectBorder = new RectangleF(1, 1, this.Width - 0.8f, this.Height - 1);
+
+            if (BorderRadius > 2) // Rounded button
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, BorderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, BorderRadius - 1F))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
+                using (Pen penBorder = new Pen(BorderColor, BorderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Inset;
+                    this.Region = new Region(pathSurface);
+
+                    // 1. DRAW BACKGROUND (The Purple Color)
+                    pevent.Graphics.FillPath(new SolidBrush(this.BackColor), pathSurface);
+
+                    // 2. DRAW SURFACE BORDER (Smooths the edges)
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+                    // 3. DRAW BUTTON BORDER (If needed)
+                    if (BorderSize >= 1)
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
+
+                    // 4. DRAW TEXT (This was missing!)
+                    // We manually draw the text on top of the new background
+                    TextRenderer.DrawText(pevent.Graphics, this.Text, this.Font, this.ClientRectangle, this.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+                }
+            }
+            else // Normal button
+            {
+                pevent.Graphics.FillRectangle(new SolidBrush(this.BackColor), rectSurface);
+                this.Region = new Region(rectSurface);
+                if (BorderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(BorderColor, BorderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+                // Draw Text for normal button too, just in case
+                TextRenderer.DrawText(pevent.Graphics, this.Text, this.Font, this.ClientRectangle, this.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            if (this.DesignMode)
+                this.Invalidate();
+        }
+    }
+}
